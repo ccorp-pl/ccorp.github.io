104f5b0c7174d860a223b7ce4efb2a045a688358
ccorp-pl <xkubek1337@gmail.com>
2024-11-07 19:07:54 +0100

    pgcwl

---
+++ script.js
@@ -0,0 +1,230 @@
+const songs = [
+    {
+        title: 'Kac KILLER!',
+        thumbnail: './assets/thumbnails/poster-1.png',
+        path: './assets/songs/music-1.mp3',
+        artist: 'DJ Bocian'
+    },
+    {
+        title: 'Bocianowe Disco',
+        thumbnail: './assets/thumbnails/poster-2.png',
+        path: './assets/songs/music-2.mp3',
+        artist: 'DJ Bocian'
+    },
+    {
+        title: 'BocianoweLove',
+        thumbnail: './assets/thumbnails/poster-3.png',
+        path: './assets/songs/music-3.mp3',
+        artist: 'DJ Bocian, DJ Maślak'
+    },
+    {
+        title: 'Prawo Ulicy',
+        thumbnail: './assets/thumbnails/poster-4.png',
+        path: './assets/songs/music-4.mp3',
+        artist: 'DJ Bocian'
+    },
+    {
+        title: 'PGCWL',
+        thumbnail: './assets/thumbnails/poster-5.png',
+        path: './assets/songs/music-5.mp3',
+        artist: 'DJ Bocian'
+    },
+];
+
+const title = document.querySelector('.title');
+const artist = document.querySelector('.artist');
+const player = document.querySelector('.player');
+const h2 = document.getElementsByTagName('h2')[0];
+let thumbnail = document.querySelector('.thumbnail');
+const progress = document.getElementById('prg');
+let songIndex = 0;
+let audio = null;
+let cplaying;
+
+(() => {
+    audio = new Audio(songs[songIndex].path);
+    audio.addEventListener('ended', () => {
+        songIndex++;
+        playnext();
+    })
+    setTitle();
+    setArtist();
+    setThumbnail();
+    setTotalDuration();
+
+    play_pause.addEventListener('click', () => {
+        setColour();
+        playSong();
+        play_pause.classList.toggle('fa-circle-pause');
+    });
+    previous.addEventListener('click', () => {
+        progress.value = 0;
+        audio.pause();
+        (songIndex == 0) ? (songIndex = songs.length - 1) : (songIndex--);
+        playaudio();
+    });
+    next.addEventListener('click', () => {
+        progress.value = 0;
+        audio.pause();
+        (songIndex == (songs.length - 1)) ? (songIndex = 0) : (songIndex++);
+        playaudio();
+    });
+})();
+
+
+function setTitle() {
+    title.innerText = songs[songIndex].title;
+}
+
+function setArtist() {
+    artist.innerText = songs[songIndex].artist;
+}
+
+function setThumbnail() {
+    thumbnail.style.background = `url(${songs[songIndex].thumbnail})`;
+    thumbnail.style.backgroundPosition = 'center center';
+    thumbnail.style.backgroundSize = 'cover';
+}
+
+function playSong() {
+    (audio.paused) ? (() => {
+        audio.play();
+        cplaying.style.animationPlayState = 'running';
+        thumbnail.style.animationPlayState = 'running';
+    })() : (() => {
+        audio.pause();
+        cplaying.style.animationPlayState = 'paused';
+        thumbnail.style.animationPlayState = 'paused';
+    })();
+
+}
+
+function setTotalDuration() {
+    let lastUpdateTime = 0;
+    current_time.innerText = '00:00';
+    audio.onloadedmetadata = () => {
+        let songlength = audio.duration;
+        let minutes = Math.floor(songlength / 60);
+        songlength %= 60;
+        let seconds = Math.floor(songlength);
+        minutes = (minutes < 10) ? ('0' + minutes) : minutes;
+        seconds = (seconds < 10) ? ('0' + seconds) : seconds;
+        total_duration.innerText = `${minutes}:${seconds}`;
+        audio.addEventListener('timeupdate', () => {
+            if ((audio.currentTime - lastUpdateTime) >= 1) {
+                updateCurrentTime(audio.duration);
+                lastUpdateTime = audio.currentTime;
+            }
+        });
+    }
+}
+
+function updateCurrentTime(songlength) {
+    let currentTime = audio.currentTime;
+    let minutes = Math.floor(currentTime / 60);
+    currentTime %= 60;
+    let seconds = Math.floor(currentTime);
+    minutes = (minutes < 10) ? ('0' + minutes) : minutes;
+    seconds = (seconds < 10) ? ('0' + seconds) : seconds;
+    current_time.innerText = `${minutes}:${seconds}`;
+    updateProgressBar(songlength, audio.currentTime);
+}
+
+function updateProgressBar(songlength, currentTime) {
+    let value = (currentTime / songlength) * 100;
+    progress.value = value;
+    progress.addEventListener('change', () => {
+        audio.currentTime = (progress.value * songlength) / 100;
+    })
+}
+
+function playnext() {
+    progress.value = 0;
+    (songIndex == (songs.length - 1)) ? (songIndex = 0) : (songIndex++);
+    let newThumbnail = thumbnail.cloneNode(true);
+    thumbnail.parentNode.replaceChild(newThumbnail, thumbnail);
+    thumbnail = newThumbnail;
+    setThumbnail();
+    setTitle();
+    setArtist();
+    audio = new Audio(songs[songIndex].path);
+    playSong();
+    setTotalDuration();
+}
+
+function playaudio() {
+    audio = new Audio(songs[songIndex].path);
+    audio.addEventListener('ended', () => {
+        playnext();
+    })
+    setTitle();
+    setArtist();
+    if (play_pause.classList.contains('fa-circle-pause')) {
+        playSong();
+        setColour();
+    }
+    setTotalDuration();
+    let newThumbnail = thumbnail.cloneNode(true);
+    thumbnail.parentNode.replaceChild(newThumbnail, thumbnail);
+    thumbnail = newThumbnail;
+    setThumbnail();
+}
+
+
+//PlayList Part
+let playlistbox = document.querySelector('.playlist');
+playlist.addEventListener('click', () => {
+    playlistbox.classList.toggle('show');
+})
+
+window.addEventListener('click', (e) => {
+    if (e.target !== playlist && e.target !== previous && e.target !== next && e.target !== play_pause && !e.target.classList.contains('songsList') && e.target !== progress) {
+        playlistbox.classList.remove('show');
+    }
+})
+
+songs.forEach((item, index) => {
+    let newdiv = document.createElement('div');
+    newdiv.classList.add('songsList');
+    newdiv.innerText = (index + 1) + '.' + ' ' + item.title;
+    playlistbox.appendChild(newdiv);
+})
+
+function setColour() {
+    let tempitem = document.querySelectorAll('.playlist div');
+    tempitem.forEach(z => {
+        z.classList.remove('currentplaying');
+    })
+    tempitem[songIndex].classList.add('currentplaying');
+    cplaying = document.querySelector('.currentplaying');
+}
+
+function playOnClick() {
+    let playlistItems = document.querySelectorAll('.playlist div');
+    playlistItems.forEach(e => {
+        e.style.cursor = 'pointer';
+        e.addEventListener('mouseover', () => {
+            e.style.opacity = '0.7';
+
+        });
+        e.addEventListener('mouseleave', () => {
+            e.style.opacity = '1';
+
+        });
+        e.addEventListener('click', () => {
+            progress.value = '0';
+            audio.pause();
+            songIndex = parseInt(e.innerText.split('.')[0]) - 1;
+            audio = new Audio(songs[songIndex].path);
+            console.log(songIndex);
+            audio.play();
+            setThumbnail();
+            setTitle();
+            setArtist();
+            setTotalDuration();
+            setColour();
+            play_pause.classList.add('fa-circle-pause');
+        })
+    })
+}
+playOnClick();
